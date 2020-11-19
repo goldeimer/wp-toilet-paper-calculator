f68d6fb126b8bcecae634e17f3a14b73401b2fa6
Johannes Pilkahn <johannes@goldeimer.de>
2020-11-19 22:40:46 +0100

    fix(wp-toilet-paper-calculator): replace window.onload w/ document ready state iife

--- src/wp-toilet-paper-calculator.js
+++ src/wp-toilet-paper-calculator.js
@@ -1,5 +1,25 @@
-import {
+import Tpc, {
+    test,
+    test2,
     renderToiletPaperCalculator
 } from '@goldeimer/toilet-paper-calculator'
 
-window.onload(() => renderToiletPaperCalculator())
+const domReady = (fn) => {
+    if (document.readyState !== 'loading') {
+        fn()
+    } else {
+        document.addEventListener('DOMContentLoaded', fn)
+    }
+}
+
+domReady(() => {
+    console.log('test')
+    console.log(test)
+    console.log('test2')
+    console.log(test2)
+    console.log('Tpc')
+    console.log(Tpc)
+    console.log('renderToiletPaperCalculator')
+    console.log(renderToiletPaperCalculator)
+    renderToiletPaperCalculator()
+})
